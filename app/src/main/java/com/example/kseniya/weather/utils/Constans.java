325421e073c4055f1666483952c427ac5c6ce868
KseniyaZharikova <kseniya.zharikova.viktorovna@gmail.com>
2018-04-19 19:09:05 +0600

    commit

--- app/src/main/java/com/example/kseniya/weather/utils/Constans.java
+++ app/src/main/java/com/example/kseniya/weather/utils/Constans.java
@@ -1,12 +1,9 @@
 package com.example.kseniya.weather.utils;
 
 public class Constans {
-    // public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
     public static final String BASE_URL = "http://dataservice.accuweather.com/";
-
     public static final String ICONS_URL = "https://developer.accuweather.com/sites/default/files/0%1s-s.png";
     public static final String ICONS_URLMORE = "https://developer.accuweather.com/sites/default/files/%s-s.png";
-    //   public static final String URL_WEATHER = "weather";
     public static final String URL_FORECAST = "forecasts/v1/daily/5day/";
 
 }
